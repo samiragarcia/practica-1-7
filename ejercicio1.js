5ebfb2a40711e5c1dd49717e7a961783e12d97c2
samira garcia <samira.garcia95@gmail.com>
2023-04-13 14:07:26 -0300

    ejercicio1

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+let manzanasMaria = 5;
+let manzanasPedro = 8;
+let manzanasSusana = 9;
+let manzanasTotales = (manzanasMaria + manzanasPedro + manzanasSusana);
+
+console.log(" maria, pedro y susana tienen " + manzanasTotales + " manzanas en la cesta");
